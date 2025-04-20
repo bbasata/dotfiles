ffee57487173dba79bf7792e745e96446dd8bc39
Baraa Basata <bbasata@gmail.com>
2025-04-20 11:32:18 -0400

    path.zsh

--- zsh/custom/path.zsh
+++ zsh/custom/path.zsh
@@ -1,7 +1,7 @@
 PATH="/opt/homebrew/opt/make/libexec/gnubin:$PATH"
 PATH="/opt/homebrew/opt/gawk/libexec/gnubin:$PATH"
 PATH="/Users/baraa.basata/.local/bin:$PATH"
-PATH="/Users/baraa.basata/go/bin:$PATH"
 PATH="/Users/baraa.basata/bin:$PATH"
+PATH="/Users/baraa.basata/go/bin:$PATH"
 
 export PATH
