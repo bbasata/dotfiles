cf741fd90833fc4e27f0d07c63cce26261655278
Baraa Basata <bbasata@gmail.com>
2025-07-14 17:14:58 -0400

    nvim

--- config/nvim/ftplugin/go.vim
+++ config/nvim/ftplugin/go.vim
@@ -3,3 +3,6 @@ setlocal tabstop=4
 
 let g:go_gopls_enabled = 1
 let g:go_template_use_pkg = 1
+
+nnoremap <Leader>1 :GoAlternate<CR>
+nnoremap <Leader>! :GoAlternate!<CR>
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,11 @@
 vim.cmd('filetype plugin indent on')
 
-vim.cmd('map <silent> <Leader>a :silent! grep! <c-r><c-w><cr>:copen<cr>')
+vim.cmd('nnoremap <silent> <Leader>a :silent! grep! <c-r><c-w><cr>:copen<cr>')
+vim.cmd('nnoremap <silent> <Leader>p :FZF<cr>')
+vim.cmd('nnoremap <silent> <Leader><Leader> :cnext<cr>')
 
+vim.g.UltiSnipsSnippetDirectories = { 'ultisnips', 'go-snippets/ultisnips' }
+
+vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.rtp:append('/opt/homebrew/opt/fzf')
