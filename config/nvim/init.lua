f58aeddd3b072110ba0ac4d4219a812009bd40a4
Baraa Basata <bbasata@gmail.com>
2025-07-18 20:42:12 -0400

    $MYVIMRC

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,11 +1,30 @@
+vim.cmd('source $VIMRUNTIME/colors/vim.lua') -- why? https://github.com/NLKNguyen/papercolor-theme/issues/201#issuecomment-2157101456
+vim.o.termguicolors = true
+vim.cmd('colorscheme torte-prime') -- also: default, lunaperche, torte, vim, papercolor-prime
 vim.cmd('filetype plugin indent on')
 
-vim.cmd('nnoremap <silent> <Leader>a :silent! grep! <c-r><c-w><cr>:copen<cr>')
-vim.cmd('nnoremap <silent> <Leader>p :FZF<cr>')
+vim.cmd('inoremap <silent> <leader>d <esc>:wall\\|:Dispatch<cr>')
+
+vim.cmd('inoremap <silent> <space><space> <esc>')
+vim.cmd('nnoremap <silent> <space><space> <esc>')
+
+vim.cmd('nnoremap <silent> <c-p> :Files .<cr>')
+
+vim.cmd('nnoremap <silent> <leader>a :silent! grep! <c-r><c-w><cr>:copen<cr>')
+
+vim.cmd('nnoremap <silent> <leader>c :silent! :Commentary<cr>')
+vim.cmd('xnoremap <silent> <leader>c <esc>:\'<,\'>Commentary<cr>')
+
+vim.cmd('nnoremap <silent> <leader>d :wall\\|:Dispatch<cr>')
+
+vim.cmd('nnoremap <silent> <leader>t :TagbarToggle<cr>')
+
 vim.cmd('nnoremap <silent> <Leader><Leader> :cnext<cr>')
 
 vim.g.UltiSnipsSnippetDirectories = { 'ultisnips', 'go-snippets/ultisnips' }
 
+vim.opt.foldmethod = 'syntax'
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.rtp:append('/opt/homebrew/opt/fzf')
+vim.opt.shortmess:append('I') -- see :intro
