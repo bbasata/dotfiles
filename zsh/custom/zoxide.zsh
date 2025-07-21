5a634a3866a1212db2bba18269607b00fec67ce3
Baraa Basata <bbasata@gmail.com>
2025-07-21 15:47:59 -0400

    zoxide.zsh

--- zsh/custom/zoxide.zsh
+++ zsh/custom/zoxide.zsh
@@ -1,2 +1,4 @@
 eval "$(zoxide init --cmd cd zsh)"
+export _ZO_EXCLUDE_DIRS="$HOME/Library/**/*:$HOME/Desktop/**/*:$HOME/Documents/**/*:$HOME/Downloads/**/*:$HOME/Music/**/*:$HOME/Movies/**/*:$HOME/Pictures/**/*:$HOME/Public/**/*:$HOME/tmp/**/*:/opt/**/*"
+export _ZO_FZF_OPTS="--bind '\:jump,jump:accept' --color 'pointer:yellow' --margin 20% --border none --tabstop 4 --scrollbar '' --cycle --no-info --separator ''"
 
