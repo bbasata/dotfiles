a921db9a201c992542448c57cfecb467f6d0564d
Baraa Basata <bbasata@gmail.com>
2025-07-20 22:52:50 -0400

    blink

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -23,7 +23,6 @@ vim.cmd('nnoremap <silent> <Leader><Leader> :cnext<cr>')
 
 vim.cmd [[set statusline=[%n]\ [%3p%%]\ %f:%l,%v\ %h%m%r\ %=%{go#statusline#Show()}\ %=%{substitute(getcwd(),$HOME,'~','')}]]
 
-
 vim.g.UltiSnipsSnippetDirectories = { 'ultisnips', 'go-snippets/ultisnips' }
 
 vim.opt.foldmethod = 'syntax'
@@ -98,6 +97,56 @@ print('😮 lsp log level: debug')
 
 vim.lsp.enable({'gopls', 'lua_ls'})
 
+-- require('nvim-treesitter').install({ 'go', 'gomod', 'gosum' ,'gotmpl', 'gowork', 'hcl', 'idris', 'lua', 'luadoc', 'python', 'ruby', 'terraform', 'yaml' })
+
+require('blink.compat').setup({ debug = true })
+
+require('cmp').register_source('go_pkgs', require('cmp_go_pkgs'))
+
 require('blink.cmp').setup({
-	completion = { documentation = { auto_show = true } },
+	completion = {
+		documentation = { auto_show = true },
+		list = { selection = { auto_insert = true, preselect = false } },
+		menu = { draw = { treesitter = { 'lsp' } } },
+		trigger = { show_on_accept_on_trigger_character = true, show_on_insert_on_trigger_character = true },
+	},
+	keymap = {
+		preset = 'super-tab'
+	},
+	signature = { enabled = true, window = { show_documentation = false } },
+	sources = {
+		default = {'lsp', 'path', 'snippets', 'buffer'},
+		per_filetype = {
+			go = { inherit_defaults = true, 'go_pkgs'}
+		},
+		providers = {
+			snippets = {
+					score_offset = 2
+			},
+			go_pkgs = {
+				name = 'go_pkgs',
+				module = 'blink.compat.source',
+				min_keyword_length = 0,
+				max_items = 11,
+
+				-- all blink.cmp source config options work as normal:
+				score_offset = 25,
+
+				-- this table is passed directly to the proxied completion source
+				-- as the `option` field in nvim-cmp's source config
+				--
+				-- this is NOT the same as the opts in a plugin's lazy.nvim spec
+				opts = {
+				}
+			}
+		}
+	}
+})
+
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+  pattern = { "*.go" },
+  callback = function(args)
+  vim.notify("LspAttack")
+    require("cmp_go_pkgs").init_items(args)
+  end,
 })
