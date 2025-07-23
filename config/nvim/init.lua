db3ada289368fa6bb2febb5d153d0adbc920e96e
Baraa Basata <bbasata@gmail.com>
2025-07-23 10:04:09 -0400

    init.lua

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,8 +1,8 @@
-vim.cmd('source $VIMRUNTIME/colors/vim.lua') -- why? https://github.com/NLKNguyen/papercolor-theme/issues/201#issuecomment-2157101456
+vim.cmd [[source $VIMRUNTIME/colors/vim.lua]] -- why? https://github.com/NLKNguyen/papercolor-theme/issues/201#issuecomment-2157101456
 vim.o.termguicolors = true
-vim.cmd('colorscheme papercolor-prime') -- also: default, lunaperche, torte, vim, torte-prime
-vim.cmd('filetype plugin indent on')
-vim.cmd('packadd nohlsearch')
+vim.cmd [[colorscheme papercolor-prime]] -- also: default, lunaperche, torte, vim, torte-prime
+vim.cmd [[filetype plugin indent on]]
+vim.cmd [[packadd nohlsearch]]
 
 require('user.options')
 require('user.mapping')
@@ -22,13 +22,11 @@ vim.lsp.enable({
 require('user.treesitter')
 require('user.completion')
 
-vim.cmd [[packadd nohlsearch]]
-
 vim.diagnostic.config({ signs = false, underline = true, update_in_insert = true, virtual_lines = true, virtual_text = false })
 
 vim.cmd [[command! -bang -nargs=? -complete=dir Files call fzf#vim#files(<q-args>, {'options': ['--cycle', '--no-info', '--color', 'pointer:yellow', '--bind', '\:jump,jump:accept']}, <bang>0)]]
 
-vim.cmd[[autocmd FileType netrw setl bufhidden=delete]]
-vim.cmd[[autocmd FileType netrw setl number]]
-vim.cmd[[autocmd FileType netrw setl relativenumber]]
+vim.cmd [[autocmd FileType netrw setl bufhidden=delete]]
+vim.cmd [[autocmd FileType netrw setl number]]
+vim.cmd [[autocmd FileType netrw setl relativenumber]]
 
