a05c8cd6219d577de545a00141995400d681a17e
Baraa Basata <bbasata@gmail.com>
2025-07-28 17:27:00 -0400

    nvim

--- config/nvim/lua/user/mapping.lua
+++ config/nvim/lua/user/mapping.lua
@@ -8,8 +8,18 @@ vim.cmd('xnoremap <silent> <leader>c <esc>:\'<,\'>Commentary<cr>')
 vim.cmd('inoremap <silent> <leader>d <esc>:wall\\|:Dispatch<cr>')
 vim.cmd('nnoremap <silent> <leader>d :wall\\|:Dispatch<cr>')
 
+vim.cmd('nnoremap <silent> <leader>gb :Git blame<cr>')
+vim.keymap.set('n', '<leader>gc', function()
+	vim.cmd [[Git]]
+	vim.api.nvim_feedkeys(':Git commit -m ""', 'nt', false)
+	local key = vim.api.nvim_replace_termcodes("<Left>", true, false, true)
+	vim.api.nvim_feedkeys(key, 'n', false)
+end, { desc = "Git commit" })
+vim.cmd('nnoremap <silent> <leader>gp :Git add -p<cr>')
+vim.cmd('nnoremap <silent> <leader>gs :Git<cr>')
+
 vim.cmd('nnoremap <silent> <leader>t :TagbarToggle<cr>')
 
 vim.cmd('nnoremap <silent> <leader>T :tabnew<cr>')
 
-vim.cmd('nnoremap <silent> <Leader><Leader> :cnext<cr>')
+vim.cmd('nnoremap <silent> <leader><leader> :cnext<cr>')
