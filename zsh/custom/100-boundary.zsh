3e5ba9e74cfc89926acbd0864c16856695175caa
Baraa Basata <bbasata@gmail.com>
2025-10-22 00:02:21 -0400

    :shipit:

--- zsh/custom/100-boundary.zsh
+++
@@ -1,6 +0,0 @@
-if [[ -x /opt/homebrew/bin/boundary ]]; then
-	autoload -U +X bashcompinit && bashcompinit
-	complete -o nospace -C /opt/homebrew/bin/boundary boundary
-fi
-
-# vim: ts=2 sw=2 sts=2
--- zsh/custom/100-terraform-provider-google-alias.zsh
+++
@@ -1,4 +0,0 @@
-
-alias mkga='VERSION=ga OUTPUT_PATH="/Users/baraa.basata/go/src/github.com/hashicorp/terraform-provider-google" make terraform'
-
-alias mkbeta='VERSION=beta OUTPUT_PATH="/Users/baraa.basata/go/src/github.com/hashicorp/terraform-provider-google-beta" make terraform'
