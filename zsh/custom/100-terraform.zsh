9e8075b638626089c38cdffc29ab5e6195ced369
Baraa Basata <bbasata@gmail.com>
2025-11-10 14:55:56 -0500

    :shipit:

--- zsh/custom/100-terraform.zsh
+++ zsh/custom/100-terraform.zsh
@@ -3,4 +3,16 @@ if [[ -x "$HOME/bin/terraform" ]]; then
 	complete -o nospace -C "$HOME/bin/terraform" terraform
 fi
 
+
+function install_terraform () {
+	version="$(curl -s https://releases.hashicorp.com/terraform/index.json | jq -r '.versions | map(.version) | .[]' | sort -V --reverse | fzf)"
+
+	if [[ $? -ne 0 ]]; then
+		echo "Have a nice day 🙂" >&2
+		return
+	fi
+
+	hc-install install -path $HOME/bin -version "$version" terraform
+}
+
 # vim: ts=2 sw=2 sts=2
