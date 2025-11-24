f82c0c381287599e8886752f7a2494d5cca4595d
Baraa Basata <bbasata@gmail.com>
2025-11-24 17:50:18 -0500

    :shipit:

--- config/nvim/lua/user/diagnostic.lua
+++ config/nvim/lua/user/diagnostic.lua
@@ -6,6 +6,20 @@ vim.diagnostic.config {
 	virtual_lines = false
 }
 
+-- vim.api.nvim_create_autocmd('DiagnosticChanged', {
+-- 	callback = function(args)
+-- 		if #args.data.diagnostics == 0 then
+-- 			vim.diagnostic.setloclist { open = false }
+-- 			vim.cmd [[
+-- 				try
+-- 				  lclose
+-- 				catch /^Vim(lclose):/
+-- 				endtry
+-- 			]]
+-- 		end
+-- 	end
+-- })
+
 vim.diagnostic.handlers.loclist = {
 	---@diagnostic disable-next-line: unused-local
 	show = function(namespace, bufnr, filtered, opts_res)
@@ -14,9 +28,19 @@ vim.diagnostic.handlers.loclist = {
 
 	---@diagnostic disable-next-line: unused-local
 	hide = function(namespace, bufnr)
-		-- neovim does not call `show` when #diagnostics is zero. so: `setloclist` to ensure that
-		-- loclist is updated, even when #diagnostics is zero
+		-- neovim appears not to call `show` when #diagnostics is zero. so: `setloclist`
+		-- to ensure that loclist is updated, even when #diagnostics is zero
+		-- open question: does this make `show` unnecessary?
 		vim.diagnostic.setloclist { open = false }
+
+		if #vim.diagnostic.count(bufnr) == 0 and vim.api.nvim_buf_is_valid(bufnr) then
+			vim.cmd [[
+				try
+					lclose
+				catch /^Vim(lclose):/
+				endtry
+			]]
+		end
 	end,
 }
 
