2940053decd6526cd2155de123df1489f1d4f552
Baraa Basata <baraa.basata@hashicorp.com>
2026-05-11 20:56:45 -0400

    🍿

--- zsh/custom/120-sandbox.zsh
+++ zsh/custom/120-sandbox.zsh
@@ -1,10 +1,10 @@
 copilot() {
-	/usr/bin/env -i "/opt/homebrew/bin/copilot" "$@"
+	/usr/bin/env -i -Popt/homebrew/bin PATH="$HOME/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin" copilot "$@"
 }
 
 
 opencode() {
-	/usr/bin/env -i "/opt/homebrew/bin/opencode" "$@"
+	/usr/bin/env -i -P/opt/homebrew/bin PATH="$HOME/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin" opencode "$@"
 }
 
 # vim: ts=2 sw=2
