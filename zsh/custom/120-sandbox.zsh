ee1747634bbcc6c58d3e2236fb4c376ae533cb9d
Baraa Basata <baraa.basata@hashicorp.com>
2026-05-21 08:12:11 -0400

    sandbox

--- zsh/custom/120-sandbox.zsh
+++ zsh/custom/120-sandbox.zsh
@@ -1,6 +1,13 @@
 copilot() {
-	/usr/bin/env -i -P/opt/homebrew/bin HOME="$HOME" PATH="$HOME/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin" copilot \
+	/usr/bin/env -i -P/opt/homebrew/bin HOME="$HOME" PATH="$HOME/go/bin:$HOME/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin" copilot \
 		--allow-tool="shell(terraform fmt),shell(terraform init),shell(terraform validate),shell(terraform providers)" \
+		"$@"
+}
+
+copilot-plan() {
+	/usr/bin/env -i -P/opt/homebrew/bin HOME="$HOME" PATH="$HOME/go/bin:$HOME/bin:/opt/homebrew/bin:/usr/bin:/bin:/usr/sbin:/sbin" copilot \
+		--allow-tool="shell(terraform:fmt),shell(terraform:init),shell(terraform:validate),shell(terraform:providers),shell(terraform:modules),shell(terraform:output)" \
+		--deny-tool="shell(terraform:apply),shell(terraform:destroy),shell(terraform:state rm),shell(terraform:state mv),shell(terraform:state push),shell(terraform:state replace-provider),shell(terraform:taint),shell(terraform:untaint)"
 		"$@"
 }
 
