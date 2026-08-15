b3a5bb2e0e199b20d6ecbcb96d8d2638502b279f
Baraa Basata <baraa.basata@hashicorp.com>
2026-08-14 22:24:37 -0400

    fzf: --height=1 for ctrl-r

--- zsh/custom/100-fzf.zsh
+++ zsh/custom/100-fzf.zsh
@@ -1,3 +1,4 @@
 # Set up fzf key bindings and fuzzy completion
 source <(fzf --zsh)
+export FZF_CTRL_R_OPTS="--height=1"
 export FZF_DEFAULT_OPTS="--bind '\\:jump,jump:accept' --color pointer:yellow --cycle --no-info"
